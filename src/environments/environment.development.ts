42795a2ec14fd750788963db495f43140168fa81
angel <angelcca2001@gmail.com>
2024-07-27 03:54:10 -0500

    feat: updated serverBasePath

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: false,
   // Server Base Path for Fake REST API
+  // https://learning-center-app-2a9fc03e17a1.herokuapp.com/api/v1
   //serverBasePath: 'http://localhost:3000/api/v1'
   // Server Base Path for Spring Boot REST API
-  serverBasePath: 'http://localhost:8080/api/v1'
+  // http://localhost:8080/api/v1
+  serverBasePath: 'https://learning-center-app-2a9fc03e17a1.herokuapp.com/api/v1'
 };
